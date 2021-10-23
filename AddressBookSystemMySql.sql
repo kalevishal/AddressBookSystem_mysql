bd6091f49ae30fd12b2dbdd6ce5bd12f01a55c8c
kalevishal <kalevishal1610@gmail.com>
2021-10-23 21:28:04 +0530

    Added: Creating new database

---
+++ AddressBookSystemMySql.sql
@@ -0,0 +1,10 @@
+***********************************************************
+      ********"Welcome to mysql database"********
+
+#UC1 - Creating Database
+
+mysql> CREATE DATABASE AddressBookService;
+Query OK, 1 row affected (0.81 sec)
+
+mysql> use AddressBookservice
+Database changed
