5517b1bcc782dd9e8b1f4f850f6a866979181d5a
kalevishal <kalevishal1610@gmail.com>
2021-10-23 21:41:08 +0530

    Added: Adding new contact in table

--- AddressBookSystemMySql.sql
+++ AddressBookSystemMySql.sql
@@ -59,6 +59,25 @@ mysql> select * from Address_Book_Table;
 +-----------+----------+-----------------+-----------+-------------+--------+-------------+--------------+
 4 rows in set (0.00 sec)
 
+#UC4 -  Editing existing contact by first name
+
+
+mysql> UPDATE Address_Book_Table
+    -> SET Address='sai sagar housing society',state='MH'
+    -> WHERE FirstName='Vishal';
+Query OK, 1 row affected (0.15 sec)
+Rows matched: 1  Changed: 1  Warnings: 0
+
+mysql> select * from Address_Book_Table;
++-----------+----------+---------------------------+-----------+-------------+--------+-------------+--------------+
+| FirstName | LastName | Address                   | City      | State       | Zip    | PhoneNumber | Email        |
++-----------+----------+---------------------------+-----------+-------------+--------+-------------+--------------+
+| Vishal    | kale     | sai sagar housing society | shirdi    | MH          | 423109 |       90215 | vk@gmail.com |
+| Utkarsh   | Mishra   | 55B,xyz colony            | zhasi     | UtarPradesh | 555104 |      778899 | um@gmail.com |
+| Shivraj   | Jadhav   | 002C,pqr colony           | solapur   | Maharashtra | 741111 |       55662 | sj@gmail.com |
+| Satish    | Gupta    | 11VB,sai colony           | hyderabad | Telangana   | 641122 |       89552 | sg@gmail.com |
++-----------+----------+---------------------------+-----------+-------------+--------+-------------+--------------+
+4 rows in set (0.02 sec)
 
 
 
